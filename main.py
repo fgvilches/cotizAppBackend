dd68a1faee9a123e927b27b7275f1e7fb666ec8d
Felipe González <felipe.gonzalez246@inacapmail.cl>
2021-08-31 14:52:28 -0400

    d

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ import os, time, subprocess, signal
 
 print("**script**: starting web")
 time.sleep(1)
-os.system('flask run &')
 print("**script**: flask server running")
+time.sleep(1)
+print("**script**: starting ngrok tcp")
+os.system('ngrok tcp -region us 5000 &')
